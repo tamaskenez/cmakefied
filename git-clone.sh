b901a7dd475d90ba3da51267fb5227f513794c6c
Tamas Kenez <tamas.kenez@adasworks.com>
2017-10-16 22:39:16 +0200

    initial commit

---
+++ git-clone.sh
@@ -0,0 +1,63 @@
+#!/bin/bash -e
+#
+# Usage:
+#
+#     git-clone.sh [--dir=<DIR>] [pkg-name]
+#
+# where
+#
+#     <DIR> is the the parent directory of the clone, default: parent dir of this repo
+#     <pkg-name> is imgui
+#
+
+cd "$(dirname $0)/.."
+deps_dir=$PWD
+cd - >/dev/null
+
+cd "$(dirname $0)"
+cmakefied_dir=$PWD
+cd - >/dev/null
+
+gitit () {
+    url="$1"
+    name="$2"
+    branch="$3"
+    dir="$deps_dir/$name"
+    if [[ ! -d "$dir" ]]; then
+        echo -e "\n-- Cloning [$name]: git clone $url $dir"
+        git clone "$url" "$dir"
+        if [[ -n "$branch" ]]; then
+            cd "$dir"
+            git checkout "$branch"
+            cd - >/dev/null
+        fi
+    else
+        cd "$dir"
+        echo -e "\n-- Updating [$name]: cd $dir && git pull --ff-only"
+        if [[ -n "$branch" ]]; then
+            git checkout "$branch"
+        fi
+        git fetch --all -p
+        git pull --ff-only
+        cd - >/dev/null
+    fi
+    cp -R "$cmakefied_dir/$2"/* "$dir"
+}
+
+while [[ $# > 0 ]]; do
+    if [[ $1 =~ --dir=(.*) ]]; then
+        deps_dir="${BASH_REMATCH[1]}"
+        shift
+        continue
+    fi
+    case $1 in
+        imgui)
+            gitit "https://github.com/ocornut/imgui.git" $1
+            ;;
+        *)
+            echo "Invalid package: $1" >&2
+            exit 1
+            ;;
+    esac
+    shift
+done
